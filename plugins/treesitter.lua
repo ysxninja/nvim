67859bc0c50e7afbd7b99bf117c991591d281e05
ya-suke <fredluki100@gmail.com>
2023-06-15 15:35:00 +0300

    add default languages for treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = {
+      "bash", "c", "dockerfile", "python",
+      "rust", "lua",
+      "go", "gomod", "gowork", "gosum",
+      "json", "json5", "jsonc",
+      "typescript", "javascript", "tsx",
+      "yaml" },
   },
 }
