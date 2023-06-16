96661a27ac8fb901bc1fd2499eacee9f1874c4e1
ya-suke <fredluki100@gmail.com>
2023-06-16 09:38:07 +0300

    replace vim-go with ray-x/go.nvim lua alternative

--- plugins/user.lua
+++ plugins/user.lua
@@ -82,15 +82,6 @@ return {
     event = "VeryLazy",
   },
   {
-    "fatih/vim-go",
-    ft = {
-      "go",
-      "gomod",
-      "gowork",
-      "gosum",
-    }
-  },
-  {
     "folke/persistence.nvim"
     ,
     event = "BufReadPre",
@@ -105,4 +96,19 @@ return {
       require('telescope').load_extension('project')
     end
   },
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", 'gomod' },
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  }
+  ,
 }
