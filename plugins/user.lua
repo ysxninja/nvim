ccdc49b8962af7f4f060056d3d4f241fcafb2bda
ya-suke <fredluki100@gmail.com>
2023-06-16 16:43:53 +0300

    add and configure dap and other plugins for golang

--- plugins/user.lua
+++ plugins/user.lua
@@ -12,18 +12,17 @@ return {
   {
     "navarasu/onedark.nvim",
     config = function()
-      require('onedark').setup {
-        style = 'dark',
+      require("onedark").setup {
+        style = "dark",
         transparent = true,
       }
-      require('onedark').load()
+      require("onedark").load()
       -- require('lualine').setup {
       --   options = {
       --     theme = 'onedark'
       --   }
       -- }
-    end
-
+    end,
   },
   {
     "vimwiki/vimwiki",
@@ -38,18 +37,18 @@ return {
           syntax = "markdown",
           ext = ".md",
           auto_diary_index = 1,
-        }
+        },
       }
       -- Use vimwiki filetype only for vimwiki files
       vim.g.vimwiki_global_ext = 0
-    end
+    end,
   },
   {
     "ThePrimeagen/vim-be-good",
     event = "VeryLazy",
   },
   {
-    "ThePrimeagen/harpoon"
+    "ThePrimeagen/harpoon",
   },
   {
     "ambv/black",
@@ -82,20 +81,18 @@ return {
     event = "VeryLazy",
   },
   {
-    "folke/persistence.nvim"
-    ,
+    "folke/persistence.nvim",
     event = "BufReadPre",
     opts = {
       -- add any custom options here
-    }
+    },
   },
   {
     "ahmedkhalf/project.nvim",
     event = "VeryLazy",
-    config = function()
-      require('telescope').load_extension('projects')
-    end
+    config = function() require("telescope").load_extension "projects" end,
   },
+  --- Go Plugins & Dap
   {
     "ray-x/go.nvim",
     dependencies = { -- optional packages
@@ -103,12 +100,15 @@ return {
       "neovim/nvim-lspconfig",
       "nvim-treesitter/nvim-treesitter",
     },
-    config = function()
-      require("go").setup()
-    end,
+    config = function() require("go").setup() end,
     event = { "CmdlineEnter" },
-    ft = { "go", 'gomod' },
-    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-  }
-  ,
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function(_, opts) require("dap-go").setup(opts) end,
+  },
 }
