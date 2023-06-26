a374848fde44256d899cf358907e4c2e20c94176
ya-suke <fredluki100@gmail.com>
2023-06-26 21:44:34 +0300

    add and cleanup mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -1,5 +1,5 @@
 -- local utils = require "user.utils"
--- local astro_utils = require "astronvim.utils"
+local astro_utils = require "astronvim.utils"
 local mark = require "harpoon.mark"
 local ui = require "harpoon.ui"
 -- Mapping data with "desc" stored directly by vim.keymap.set().
@@ -29,6 +29,28 @@ local mappings = {
       function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
       desc = "Previous buffer",
     },
+    -- better search
+    -- n = { utils.better_search "n", desc = "Next search" },
+    -- N = { utils.better_search "N", desc = "Previous search" },
+    -- better increment/decrement
+    ["-"] = { "<c-x>", desc = "Descrement number" },
+    ["+"] = { "<c-a>", desc = "Increment number" },
+    -- resize with arrows
+    ["<Up>"] = { function() require("smart-splits").resize_up(2) end, desc = "Resize split up" },
+    ["<Down>"] = { function() require("smart-splits").resize_down(2) end, desc = "Resize split down" },
+    ["<Left>"] = { function() require("smart-splits").resize_left(2) end, desc = "Resize split left" },
+    ["<Right>"] = { function() require("smart-splits").resize_right(2) end, desc = "Resize split right" },
+    -- buffer switching
+    ["<Tab>"] = {
+      function()
+        if #vim.t.bufs > 1 then
+          require("telescope.builtin").buffers { sort_mru = true, ignore_current_buffer = true }
+        else
+          astro_utils.notify "No other buffers open"
+        end
+      end,
+      desc = "Switch Buffers",
+    },
     -- -- vim-sandwich
     -- ["s"] = "<Nop>",
     ["<leader>n"] = { "<cmd>enew<cr>", desc = "New File" },
@@ -36,12 +58,6 @@ local mappings = {
     ["<leader><cr>"] = { '<esc>/<++><cr>"_c4l', desc = "Next Template Using <++>" },
 
     ["<leader>."] = { "<cmd>cd %:p:h<cr>", desc = "Set CWD" },
-    -- better search
-    -- n = { utils.better_search "n", desc = "Next search" },
-    -- N = { utils.better_search "N", desc = "Previous search" },
-    -- better increment/decrement
-    ["-"] = { "<c-x>", desc = "Descrement number" },
-    ["+"] = { "<c-a>", desc = "Increment number" },
     -- Trouble Diagnostics
     ["<leader>x"] = { desc = "󰒡 Trouble" },
     ["<leader>xx"] = { "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
@@ -101,6 +117,11 @@ local mappings = {
     ["<leader>d"] = { [["_d]], desc = "Delete and send to abyss" },
     -- copy without sending to system clipboard
     ["<leader>y"] = { [["+y]] },
+  },
+  i = {
+    -- signature help, fails silently so attach always
+    ["<C-l>"] = { function() vim.lsp.buf.signature_help() end, desc = "Signature help" },
+    ["<S-Tab>"] = { "<C-V><Tab>", desc = "Tab character" },
   },
   -- Visual
   x = {
