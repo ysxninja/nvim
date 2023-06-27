66143199c1adec450523217206ce2a0bedafa8ed
ya-suke <fredluki100@gmail.com>
2023-06-27 13:08:14 +0300

    add new user config for ray-x/go.nvim and cleanup

--- plugins/user.lua
+++ plugins/user.lua
@@ -125,8 +125,8 @@ return {
       "neovim/nvim-lspconfig",
       "nvim-treesitter/nvim-treesitter",
     },
-    config = function() require("go").setup() end,
-    -- event = { "CmdlineEnter" },
+    config = function(_, opts) require("go").setup(opts) end,
+    event = { "CmdlineEnter" },
     ft = { "go", "gomod" },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
@@ -139,6 +139,7 @@ return {
   -- Snippets
   {
     "rafamadriz/friendly-snippets",
+    -- enabled = false,
   },
   -- INFO: Vim Tmux integration Plugins
   {
