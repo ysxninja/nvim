ff4c271eb09356ccdb1178a96ff967b2944e4599
ya-suke <fredluki100@gmail.com>
2023-06-27 13:34:59 +0300

    add file diagnostics.lua

--- plugins/noice.lua
+++ plugins/noice.lua
@@ -5,16 +5,25 @@ return {
     event = "VeryLazy",
     opts = {
       cmdline = { view = "cmdline" },
-      messages = { view_search = false },
+      messages = {
+        -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+        -- This is a current Neovim limitation.
+        enabled = true, -- enables the Noice messages UI
+        view = "notify", -- default view for messages
+        view_error = "notify", -- view for errors
+        view_warn = "notify", -- view for warnings
+        view_history = "messages", -- view for :messages
+        view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+      },
       lsp = {
         progress = { enabled = false },
         hover = { enabled = false },
         signature = { enabled = false },
       },
       routes = {
-        { filter = { event = "msg_show", find = "%d+L,%s%d+B" },        opts = { skip = true } }, -- skip save notifications
-        { filter = { event = "msg_show", find = "^%d+ more lines$" },   opts = { skip = true } }, -- skip paste notifications
-        { filter = { event = "msg_show", find = "^%d+ fewer lines$" },  opts = { skip = true } }, -- skip delete notifications
+        { filter = { event = "msg_show", find = "%d+L,%s%d+B" }, opts = { skip = true } }, -- skip save notifications
+        { filter = { event = "msg_show", find = "^%d+ more lines$" }, opts = { skip = true } }, -- skip paste notifications
+        { filter = { event = "msg_show", find = "^%d+ fewer lines$" }, opts = { skip = true } }, -- skip delete notifications
         { filter = { event = "msg_show", find = "^%d+ lines yanked$" }, opts = { skip = true } }, -- skip yank notifications
       },
       presets = { long_message_to_split = true },
