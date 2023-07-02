33201a49b415ce5fbb81c0d17e3194b8d404e739
ya-suke <fredluki100@gmail.com>
2023-07-02 21:55:06 +0300

    disable orgmode for neorg

--- plugins/orgmode.lua
+++ plugins/orgmode.lua
@@ -1,40 +1,40 @@
 return {
-  {
-    "nvim-orgmode/orgmode",
-    ft = { "org" },
-    dependencies = { "akinsho/org-bullets.nvim", "lukas-reineke/headlines.nvim" },
-    config = function()
-      require("orgmode").setup {}
-      -- init.lua
-
-      -- Load custom treesitter grammar for org filetype
-      require("orgmode").setup_ts_grammar()
-
-      -- Treesitter configuration
-      require("nvim-treesitter.configs").setup {
-        -- If TS highlights are not enabled at all, or disabled via `disable` prop,
-        -- highlighting will fallback to default Vim syntax highlighting
-        highlight = {
-          enable = true,
-          -- Required for spellcheck, some LaTex highlights and
-          -- code block highlights that do not have ts grammar
-          additional_vim_regex_highlighting = { "org" },
-        },
-        ensure_installed = { "org" }, -- Or run :TSUpdate org
-      }
-
-      require("orgmode").setup {
-        org_agenda_files = { "~/Dropbox/org/*", "~/my-orgs/**/*" },
-        org_default_notes_file = "~/Dropbox/org/refile.org",
-      }
-
-      require("org-bullets").setup {}
-    end,
-    lazy = false,
-  },
-  {
-    "lukas-reineke/headlines.nvim",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    config = true,
-  },
+  -- {
+  --   "nvim-orgmode/orgmode",
+  --   ft = { "org" },
+  --   dependencies = { "akinsho/org-bullets.nvim", "lukas-reineke/headlines.nvim" },
+  --   config = function()
+  --     require("orgmode").setup {}
+  --     -- init.lua
+  --
+  --     -- Load custom treesitter grammar for org filetype
+  --     require("orgmode").setup_ts_grammar()
+  --
+  --     -- Treesitter configuration
+  --     require("nvim-treesitter.configs").setup {
+  --       -- If TS highlights are not enabled at all, or disabled via `disable` prop,
+  --       -- highlighting will fallback to default Vim syntax highlighting
+  --       highlight = {
+  --         enable = true,
+  --         -- Required for spellcheck, some LaTex highlights and
+  --         -- code block highlights that do not have ts grammar
+  --         additional_vim_regex_highlighting = { "org" },
+  --       },
+  --       ensure_installed = { "org" }, -- Or run :TSUpdate org
+  --     }
+  --
+  --     require("orgmode").setup {
+  --       org_agenda_files = { "~/Dropbox/org/*", "~/my-orgs/**/*" },
+  --       org_default_notes_file = "~/Dropbox/org/refile.org",
+  --     }
+  --
+  --     require("org-bullets").setup {}
+  --   end,
+  --   lazy = false,
+  -- },
+  -- {
+  --   "lukas-reineke/headlines.nvim",
+  --   dependencies = "nvim-treesitter/nvim-treesitter",
+  --   config = true,
+  -- },
 }
