8818bd1cafb0769d581067cd457be7e53b7f23c5
ya-suke <fredluki100@gmail.com>
2024-01-04 20:11:47 +0300

    remove notify clutter

--- plugins/noice.lua
+++ plugins/noice.lua
@@ -1,8 +1,15 @@
 return {
-  { "rcarriga/nvim-notify", init = false, config = true },
   {
     "folke/noice.nvim",
     event = "VeryLazy",
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      -- `nvim-notify` is only needed, if you want to use the notification view.
+      -- If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
     opts = {
       cmdline = {
         view = "cmdline",
@@ -14,9 +21,9 @@ return {
         -- NOTE: If you enable messages, then the cmdline is enabled automatically.
         -- This is a current Neovim limitation.
         enabled = true,              -- enables the Noice messages UI
-        view = "notify",             -- default view for messages
-        view_error = "notify",       -- view for errors
-        view_warn = "notify",        -- view for warnings
+        view = "virtualtext",             -- default view for messages
+        view_error = "virtualtext",       -- view for errors
+        view_warn = "virtualtext",        -- view for warnings
         view_history = "messages",   -- view for :messages
         view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
       },
