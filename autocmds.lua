848ccdc313fede64a9967cc5cb441e8edc937a8a
ya-suke <fredluki100@gmail.com>
2024-05-22 21:41:49 +0300

    Configure neorg @document.meta autofold

--- autocmds.lua
+++ autocmds.lua
@@ -7,3 +7,33 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = true
   end,
 })
+
+-- Autocommand to set up folding for Neorg files
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "norg",
+    callback = function()
+        -- Set folding method to Treesitter
+        vim.opt_local.foldmethod = 'expr'
+        vim.opt_local.foldexpr = 'nvim_treesitter#foldexpr()'
+        vim.opt_local.foldenable = true
+        vim.opt_local.foldlevelstart = 99
+
+        -- Function to fold `@document.meta` section
+        local function fold_meta()
+            local bufnr = vim.fn.bufnr()
+            local meta_start = vim.fn.search('@document.meta', 'nw')
+            if meta_start > 0 then
+                vim.fn.setpos('.', {bufnr, meta_start, 1, 0})
+                vim.cmd('normal! zc')
+                -- Move to the last cursor position
+                vim.cmd('normal! g`"')
+            end
+        end
+
+        -- Call the fold_meta function on BufWinEnter event
+        vim.api.nvim_create_autocmd("BufWinEnter", {
+            buffer = 0, -- Apply to the current buffer
+            callback = fold_meta
+        })
+    end,
+})
