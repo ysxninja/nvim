a3d46482cd7e3822f111b482ac492c3092f45029
ya-suke <fredluki100@gmail.com>
2024-05-24 11:57:20 +0300

    Setup and configure orgmode

--- plugins/orgmode.lua
+++ plugins/orgmode.lua
@@ -9,6 +9,7 @@ return {
     require('orgmode').setup({
       org_agenda_files = '~/orgfiles/**/*',
       org_default_notes_file = '~/orgfiles/refile.org',
+      org_startup_folded = 'content',
     })
 
   end,
