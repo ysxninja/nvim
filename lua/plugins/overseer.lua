170f7e0255ecaf424934f65d7ed766c725e533f7
ya-suke <fredluki100@gmail.com>
2025-03-08 19:29:52 +0300

    add overseer easy-align, update plugins

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -96,7 +96,7 @@ return {
         },
         mappings = {
           n = {
-            [prefix] = { desc = "яТа Overseer" },
+            [prefix] = { desc = " Overseer" },
             [prefix .. "a"] = { "<Cmd>OverseerQuickAction<CR>", desc = "Quick Action" },
             [prefix .. "i"] = { "<Cmd>OverseerInfo<CR>", desc = "Overseer Info" },
             [prefix .. "k"] = { "<Cmd>Compile<CR>", desc = "Compile" },
