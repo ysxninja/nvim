1bfd3661886f926a541e8f11996f2b99a83978fc
ya-suke <fredluki100@gmail.com>
2025-03-09 14:50:45 +0300

    update plugins

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -23,7 +23,7 @@ return {
         -- This is a current Neovim limitation.
         enabled = true, -- enables the Noice messages UI
         view = "mini", -- default view for messages
-        view_error = "messages", -- view for errors
+        view_error = "mini", -- view for errors
         view_warn = "mini", -- view for warnings
         view_history = "messages", -- view for :messages
         view_search = false, -- view for search count messages. virtualtext, Set to `false` to disable
