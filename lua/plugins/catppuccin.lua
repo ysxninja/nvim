1d7beac5bcf52cf13aea4affcebdd7b1d027cd22
ya-suke <fredluki100@gmail.com>
2025-03-09 15:04:14 +0300

    update plugins

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,48 +2,56 @@
 return {
   "catppuccin/nvim",
   name = "catppuccin",
-  priority = 1000,
+  ---@type CatppuccinOptions
   opts = {
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
-    background = {     -- :h background
-      light = "latte",
-      dark = "mocha",
-    },
-    transparent_background = false, -- disables setting the background color.
-    show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
-    term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
-    dim_inactive = {
-      enabled = false,              -- dims the background color of inactive window
-      shade = "dark",
-      percentage = 0.15,            -- percentage of the shade to apply to the inactive window
-    },
-    no_italic = false,              -- Force no italic
-    no_bold = false,                -- Force no bold
-    no_underline = false,           -- Force no underline
-    styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
-      comments = { "italic" },      -- Change the style of comments
-      conditionals = { "italic" },
-      loops = {},
-      functions = {},
-      keywords = {},
-      strings = {},
-      variables = {},
-      numbers = {},
-      booleans = {},
-      properties = {},
-      types = {},
-      operators = {},
-    },
-    color_overrides = {},
-    custom_highlights = {},
+    dim_inactive = { enabled = true, percentage = 0.25 },
+    default_integrations = false,
     integrations = {
+      aerial = true,
       cmp = true,
+      dap = true,
+      dap_ui = true,
       gitsigns = true,
-      nvimtree = true,
-      telescope = true,
+      illuminate = { enabled = true, lsp = true },
+      indent_blankline = true,
+      markdown = true,
+      mason = true,
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
+        },
+        underlines = {
+          errors = { "underline" },
+          hints = { "underline" },
+          warnings = { "underline" },
+          information = { "underline" },
+        },
+        inlay_hints = { background = false },
+      },
+      neotree = true,
       notify = true,
-      mini = false,
-      -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+      semantic_tokens = true,
+      symbols_outline = true,
+      telescope = { enabled = true, style = "nvchad" },
+      treesitter = true,
+      ufo = true,
+      which_key = true,
+      window_picker = true,
+    },
+    custom_highlights = {
+      -- disable italics  for treesitter highlights
+      LspInlayHint = { style = { "italic" } },
+      UfoFoldedEllipsis = { link = "UfoFoldedFg" },
+      ["@parameter"] = { style = {} },
+      ["@type.builtin"] = { style = {} },
+      ["@namespace"] = { style = {} },
+      ["@text.uri"] = { style = { "underline" } },
+      ["@tag.attribute"] = { style = {} },
+      ["@tag.attribute.tsx"] = { style = {} },
     },
   },
 }
